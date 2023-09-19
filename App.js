ba428ea15627276a91e0995a77c17818a0e69d0b
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-09-19 16:08:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.css';
+function App(){
+   return(
+      <div className="App">
+         <h1>Getting input byt filling box:</h1>
+         <input type="text" />
+      </div>
+   );
+   }
+export default App;
